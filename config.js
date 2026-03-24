39303e69b92b959c574236a0c200d1710515a469
zekkek012345-source <zekkek012345@gmail.com>
2026-03-24 19:16:02 +0200

    Set Apps Script endpoint

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Paste your Google Apps Script Web App URL below.
 // Example:
 // window.LEADPULSE_ENDPOINT = "https://script.google.com/macros/s/AKfycb.../exec";
-window.LEADPULSE_ENDPOINT = "";
+window.LEADPULSE_ENDPOINT = "https://script.google.com/macros/s/AKfycby-UP-JwAGJLfJGNPTZrkXlIFZ-HpMnpQIcRzgFQ9HtRuN91Astm_6Q6rHMCXZB9dpZ/exec";
