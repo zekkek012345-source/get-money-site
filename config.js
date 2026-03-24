7e2de41eb39df7d301d8c4bde808447ac917061c
zekkek012345-source <zekkek012345@gmail.com>
2026-03-24 19:03:11 +0200

    Update config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+// Paste your Google Apps Script Web App URL below.
+// Example:
+// window.LEADPULSE_ENDPOINT = "https://script.google.com/macros/s/AKfycb.../exec";
+window.LEADPULSE_ENDPOINT = "";
